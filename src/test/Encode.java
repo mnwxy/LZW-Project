4e24d87ab1859ab66526cfc4b5cf5176649be24c
Red4D <matthewwilliamredford@gmail.com>
2020-09-11 09:30:18 -0700

    fix(encode): fixed fencepost error in encoding and added file I/O
    File Output
    
    Changes output from println to a separate file.
    
    feat: implemented lzw decoding
    
    docs(decode): added comments and improved variable names to Encode.java
    
    Variable Names and Comments
    
    Added more documentation to Encode.java to improve readability.

--- src/test/Encode.java
+++ src/test/Encode.java
@@ -5,29 +5,32 @@ import java.io.*;
 
 public class Encode
 {
-	//this main function was our tester. when the string "abcabcabcabcabcabcabcabcabcabcabcabc" is in yolo.txt, it prints the following:
-	//"97 98 99 257 259 258 260 263 262 265 261 267 264 99"
-//	public static void main(String[] args) throws IOException
-//	{
-//		System.out.println(encode("yolo.txt"));
-//	}
+	//takes in "yolo.txt" and outputs the encoded "yolo.txt.lzw"
+	public static void main(String[] args) throws IOException
+	{
+		File encodedFile = new File("yolo.txt.lzw");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(encodedFile));
+		writer.write(encode("yolo.txt"));
+		writer.close();
+	}
 	//takes in a filename, returns a string with the integers representing the codes delimited by spaces. does not return a bitstream.
 	public static String encode(String filename) throws IOException
 	{
-		String file = LZWHelper.readFile(filename);
-		HashMap dictionary = new HashMap();
+		String inputFile = LZWHelper.readFile(filename);
+		HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
 		String previous = "";
-		String current = file.substring(0,1);
+		String current = inputFile.substring(0,1);
 		String combined = previous+current;
 		//this value is the size of our initial dictionary.
-		int value = 256;
+		int value = 255;
 		String output = "";
+		String tableOutput = "";
 		//building our ASCII dictionary
-		for(int i = 0; i < 255; i++)
+		for(int i = 0; i < 256; i++)
 		{
 			dictionary.put((char)(i)+"", i);
 		}
-		for(int i=1;i<file.length()+1;i++)
+		for(int i=1;i<inputFile.length()+1;i++)
 		{
 			combined = previous+current;
 			//if the length of the character is 1, then it should already be in our dictionary; no need to waste time checking.
@@ -36,7 +39,7 @@ public class Encode
 				
 				previous = previous + current;
 				//this patches the edge case when the code has reached the end of the string
-				if(i == file.length())
+				if(i == inputFile.length())
 				{
 					output = output + " " + ((int)dictionary.get(previous));
 				}
@@ -48,20 +51,31 @@ public class Encode
 				value = value + 1;
 				dictionary.put(combined,value);
 				//this patches the edge case when the code has reached the end of the string
-				if(i == file.length())
+				if(i == inputFile.length())
 				{
-					output = output + " " + (int)(file.charAt(file.length()-1));
+					output = output + " " + (int)(inputFile.charAt(inputFile.length()-1));
 				}
 				previous = current;
 			}
 			//this conditional is necessary because i runs from 1 to file.length().
-			if(i < file.length())
+			if(i < inputFile.length())
 			{
-				current = file.substring(i,i+1);
+				current = inputFile.substring(i,i+1);
 			}
 		}
-		return output;
+		//this concatenates the dictionary into a single string called "tableOutput"
+		int length = dictionary.size();
+		String[] table = new String[length];
+		for(Map.Entry<String, Integer> Entry: dictionary.entrySet())
+		{
+			table[Entry.getValue()] = Entry.getKey();
+		}
+		for(int i=256; i<table.length; i++)
+		{
+			tableOutput += table[i] + " ";
+		}
+		tableOutput = tableOutput.substring(0, tableOutput.length() - 1);
+		//this returns the contents of both tableOutput and the encoded output
+		return ("" + (length - 256) + " " + tableOutput + output);
 	}
-		
-		
 }
