bf60d3ad430269ddf3f356cc60ee462896d9d01a
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-09-15 14:48:25 -0700

    docs(decode): added comments and improved variable names to Encode.java

--- src/test/Decode.java
+++ src/test/Decode.java
@@ -4,43 +4,50 @@ import java.util.*;
 import java.io.*;
 
 public class Decode
-{
-	//this main function was our tester. when the string "abcabcabcabcabcabcabcabcabcabcabcabc" is in yolo.txt, it prints the following:
-		//"97 98 99 257 259 258 260 263 262 265 261 267 264 99"
+{		
 		public static void main(String[] args) throws IOException
 		{
 			System.out.println(decode("yolo.txt.lzw"));
 		}
-		//takes in a filename, returns a string with the integers representing the codes delimited by spaces. does not return a bitstream.
+		
+		/**
+		 * Decompresses a file encoded with Tao-Zhang implementation 
+		 * of Lempel-Ziv-Welch encoding.
+		 * 
+		 * @param filename Path to file with compressed message.
+		 * @return Decompressed message
+		 * @throws IOException
+		 */
 		public static String decode(String filename) throws IOException
 		{
-			String file = LZWHelper.readFile(filename);
+			String fileContents = LZWHelper.readFile(filename);
 			HashMap<Integer, String> dictionary = new HashMap<Integer, String>();
+			String reconstructed = "";
 			
-			//this value is the size of our initial dictionary.
-			String output = "";
-			
-			//building our ASCII dictionary
+			// Populate dictionary with mapping of ASCII value (as a 32-bit integer) 
+			// to its String representation.
+			// e.g.
+			// 0x0A -> "\n"
+			// 0x41 -> "A"
 			for(int i = 0; i < 256; i++)
 			{
 				dictionary.put(i, (char)(i)+"");
-			}
+			}						
 			
-			String reconstructed = "";
+			// Tokenized message from contents of file.
+			String[] tokens = fileContents.split(" ");
 			
-			// Number of new characters
-			String[] stream = file.split(" ");
-			
-			int count = Integer.parseInt(stream[0]);
+			// Number of extra entries in dictionary mapping.
+			int count = Integer.parseInt(tokens[0]);
 			
 			for (int i = 1; i < 1 + count; ++i)
 			{
-				dictionary.put(i + 255, stream[i]);
+				dictionary.put(i + 255, tokens[i]);
 			}
 			
-			for(int i = 1 + count; i < stream.length; ++i)
+			for(int i = 1 + count; i < tokens.length; ++i)
 			{
-				reconstructed += dictionary.get(Integer.parseInt(stream[i]));
+				reconstructed += dictionary.get(Integer.parseInt(tokens[i]));
 			}
 			
 			return reconstructed;
